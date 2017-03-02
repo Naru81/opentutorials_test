0e4bd97bf4853fb64263aa7ad5a72b93b9874c3d
조용찬 <yccho@neowiz.com>
2017-03-02 18:24:39 +0900

    커밋 Test

---
+++ Assets/Scripts/TestManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class TestManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnGUI()
+    {
+		
+    }
+}
